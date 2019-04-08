6db57da3da1eda8486a54fb1271b06cb97d265c7
hejiale <jiale.he@mail.hypers.com>
2019-04-08 13:16:39 +0800

    init Project

---
+++ src/main/java/GitTest_java.java
@@ -0,0 +1,10 @@
+/**
+ * @author jiale.he
+ * @date 2019/04/08
+ * @email jiale.he@mail.hypers.com
+ */
+public class GitTest_java {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
---
+++ src/main/scala/com/GitTest.scala
@@ -0,0 +1,13 @@
+package com
+
+/**
+  *
+  * @author jiale.he
+  * @date 2019/04/08
+  * @email jiale.he@mail.hypers.com
+  */
+object GitTest {
+  def main(args: Array[String]): Unit = {
+    println(11)
+  }
+}
